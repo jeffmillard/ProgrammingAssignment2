fff24b819562c50a72f79e07dc765868ebd8b6e0
jeffmillard <jmillard@cox.net>
2014-04-23 10:55:10 -0700

    First working version, tested against a simple 2x2 matrix.  Cache message displayed after initial trial, so seems to be in working order.

--- cachematrix.R
+++ cachematrix.R
@@ -1,64 +1,118 @@
-## =============================================================
+## =============================================================================
+## Function:	makeCacheMatrix <- function(x = matrix())
+## -----------------------------------------------------------------------------
+## Description:	Function that results in a list of functions
+##			for storing and retrieving a matrix and its inverse
+##			resulting a a matrix "object" with those functions
+##
+## Details: (with apologies for verbosity, trying to understand it all myself)
+	# 1. Declare a function that takes a matrix x as an argument
+	# 2. Initalize variable matrix m as null
+	# 3. Create a set "method" that gets the argument passed to 
+	#	the function (x)
+	# 4. Create a get "method" that ... (and uses x how ?)
+	# 5. Create a setinvert "method" that inverts a square matrix 
+	#	using solve()
+	# 6. Returns a list of these functions as a factory of sorts to
+	#	make "cacheable" matrices which have these functions
 
-## Write a short comment describing this function
-## =============================================================
-
+## Arguments:	matrix 
+##
+## Returns: 	List of functions: set, get, setinvert, getinvert
+##
+## Usage:		
+	# 	create a test matrix
+	#		z <- matrix(1:4,2)
+	#	create an "augmented" matrix/list
+	#		zplus <- makeCacheMatrix(z)
+	#	this could be shortened to
+	#		zplus <- makeCacheMatrix(matrix(1:4,2))
+	#	but it is nice to be able to test the result using
+	#		z %*% cacheSolve(zplus)
+## =============================================================================
 makeCacheMatrix <- function(x = matrix()) {
-#        makeVector <- function(x = numeric()) {
-#                m <- NULL
-#                set <- function(y) {
-#                        x <<- y
-#                        m <<- NULL
-#                }
-#                get <- function() x
-#                setmean <- function(mean) m <<- mean
-#                getmean <- function() m
-#                list(set = set, get = get,
-#                     setmean = setmean,
-#                     getmean = getmean)
-#        }
-        
+	
+	# Initialize matrix m (to hold the matrix inversion of x)
+	m <- NULL
+	
+	# Define "set" function for re-initializing m and storing x
+	# Reset m as null, and set (persist) the matrix x and inverse mx 
+	# in the parent environment, both using the '<<-' assignment
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	
+	# define "get" function for getting (reading in) the matrix x
+	get <- function() x
+	
+	# define "setinverse" function to invert the matrix and caching it
+	# per hint on forums, assume square matrix and invert using solve()
+	
+	# the <<- assignment performs the caching by persisting the variable
+	#   m in the parent environment of the function
+	setinverse <- function(solve) m <<- solve
+	
+	# define the "getinvert" function, which simply tries to retrieve m
+	# thus returning either a cached matrix or NULL
+	getinverse <- function() m
+	
+	# return a list of the functions
+	list( set = set, 
+		get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
+ 
 }
 
+## =============================================================================
+## Function:	cacheSolve <- function(x, ...) 
+## -----------------------------------------------------------------------------
+## Description:	Return the inverse of a square matrix, either from a cached
+##			result, or if one is not available, computing and caching 
+##			the inverse
+##
+## Details: (more verbosity)
+	# 1. Declare a function that takes a matrix x as an argument
+	# 2. See if the inverted matrix is already available (cached) using
+	#	getinverse() function from makeCacheMatrix
+	# 3. If no cached result exists, 
+	#	a. Read the original matrix from parent environment 
+	#		using get() function from makeCacheMatrix
+	#	b. Compute the inverse
+	#	c. Cache the inverse in the parent environment using
+	#		setinverse() function from makeCacheMatrix 
+	# 4. Return the inverted matrix 
 
-## =============================================================
-## Function:    cacheSolve <- function(x, ...) 
-## 
-## Description: Return a matrix that is the inverse of 'x'
+## Arguments:	"matrix" (list) created by makeCacheMatrix()
 ##
-## Arguments:   x
+## Returns: 	Matrix inverse of argument
 ##
-## Returns:
-##
-## Notes:       
-
+## Usage:		See above makeCacheMatrix()
+## =============================================================================
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of matrix x
         
-        ## replace with inverse ??
-        m <- x$getmean() # not mean, change this
+        # try getting the cached inverse (A), if it exists
+        m <- x$getinverse()
         
-        ## probably again check if it is not null
+        # if it does exist (not null), read it and return that matrix
+        # and exit function
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         
-        ## if it is null, calculate inverse
+        # if it is null (not cached), calculate the inverse
+        # first, read the original matrix (passed as argument) 
         data <- x$get()
-        m <- mean(data, ...) # not mean, change
-        x$setmean(m)
+        
+        # invert
+        m <- solve(data)
+        
+        # cache the result for future use
+        x$setinverse(m)
+        
+        # return the inverse
         m
 }       
-        cachemean <- function(x, ...) {
-                m <- x$getmean()
-                if(!is.null(m)) {
-                        message("getting cached data")
-                        return(m)
-                }
-                data <- x$get()
-                m <- mean(data, ...)
-                x$setmean(m)
-                m
-        }
-}
